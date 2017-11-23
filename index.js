d94e4b9037910e0a4817e3634a90937deef16395
Paul Esson <paul.esson@blueflag.com.au>
2017-11-23 22:27:42 +1100

    Fix so that this works

--- index.js
+++ index.js
@@ -1,27 +1,39 @@
-const Telnet = require('telnet-client');
+// const Telnet = require('telnet-client');
 const ping = require('ping');
 
 const PING_HOST = 'www.google.com';
-const TELNET_HOST = 'Optus.AC800S';
-const TELNET_PORT = 5510
+const TELNET_HOST = '192.168.1.1';
+const TELNET_PORT = 5510;
 const AT_COMMAND = 'AT!GSTATUS?';
 
-async function printBand(){
-  let connection = new Telnet();
-  let params = {
-    host: TELNET_HOST,
-    port: TELNET_PORT,
-    timeout: 1500
-  }
-  await connection.connect(params);
-  let res =  await connection.exec(AT_COMMAND)
-  console.log(AT_COMMAND, new Date().toISOString(), res);
-}
+
+const Telnet = require('telnet-rxjs').Telnet;
+const client = Telnet.client(`${TELNET_HOST}:${TELNET_PORT}`);
+
+
+var connected = 
+  new Promise((resolve) => {
+    client.filter((event) => event instanceof Telnet.Event.Connected).subscribe((event) => {
+        resolve();  
+    });
+})
+
+
+client.data
+  .subscribe((data) => {
+      console.log(data);
+  });
+
+client.connect();
 
 function pingHost(){
    ping.sys.probe(PING_HOST, (up) => {
   	console.log('is up', up, new Date().toISOString());
   })
+}
+
+function printBand(){
+  connected.then(() => client.sendln(AT_COMMAND))
 }
 
 function intervalFunc() {
@@ -30,3 +42,4 @@ function intervalFunc() {
 }
 
 setInterval(intervalFunc, 1500);
+
