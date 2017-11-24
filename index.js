72cd7b9c0e0b7b5129030ba15c6b0b21894f6f13
Paul Esson <paul.esson@blueflag.com.au>
2017-11-24 15:35:18 +1100

    update so that we do https requests, since this seems to cause problems

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // const Telnet = require('telnet-client');
 const ping = require('ping');
-
+const fetch = require('node-fetch');
 const PING_HOST = 'www.google.com';
 const TELNET_HOST = '192.168.1.1';
 const TELNET_PORT = 5510;
@@ -36,9 +36,31 @@ function printBand(){
   connected.then(() => client.sendln(AT_COMMAND))
 }
 
+function requestSitePrintIfFail(){
+  Promise.all([
+    fetch('https://github.com/thepont'),
+    fetch('https://www.theregister.co.uk/'),
+    fetch('https://www.google.com.au/'),
+    fetch('https://yescrowd.optus.com.au/'),
+    fetch('https://yahoo.com/'),
+    fetch('https://placekitten.com/200/200?image=11')
+  ])
+  .catch(err => {
+    console.log(err);
+    printBand();
+  })
+  //.then(resp => {
+  //  resp.forEach(ii => {
+  //    console.log(ii.status);
+  //    console.log(ii.url);
+  //  })
+  //})
+}
+
 function intervalFunc() {
-  pingHost();
-  printBand();
+  requestSitePrintIfFail();
+  //pingHost();
+  //printBand();
 }
 
 setInterval(intervalFunc, 1500);
